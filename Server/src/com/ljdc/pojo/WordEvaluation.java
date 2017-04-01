9791675394faf9b1c799e3ea65cb4532c03f07a5
邹旭 <frank_zouxu@163.com>
2017-04-01 18:18:48 +0800

    服务器端：    加入词汇评估功能中的          -> 词汇预估表（涵盖用于词汇预估的词汇）          -> 将POJO加入到配置文件中          -> 加入到同步管理中

---
+++ Server/src/com/ljdc/pojo/WordEvaluation.java
@@ -0,0 +1,74 @@
+package com.ljdc.pojo;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+/**
+ * Created with IntelliJ IDEA
+ * User:邹旭
+ * Date:2017/2/22 0022
+ * Time:下午 7:57
+ * Desc:略
+ */
+@Entity
+@Table(name = "lib1", schema = "server_ljdc", catalog = "")
+public class Lib1EnglishGrand4CoreServer {
+    private int lib1Id;
+    private Collection<LearnLib1Server> learnLib1;
+    private WordLibServer wordLib;
+
+    public Lib1EnglishGrand4CoreServer() {
+    }
+
+    public Lib1EnglishGrand4CoreServer(int lib1Id) {
+        this.lib1Id = lib1Id;
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "lib1Id", nullable = false)
+    public int getLib1Id() {
+        return lib1Id;
+    }
+
+    public void setLib1Id(int lib1Id) {
+        this.lib1Id = lib1Id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Lib1EnglishGrand4CoreServer that = (Lib1EnglishGrand4CoreServer) o;
+
+        if (lib1Id != that.lib1Id) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return lib1Id;
+    }
+
+    @OneToMany(mappedBy = "lib1")
+    public Collection<LearnLib1Server> getLearnLib1() {
+        return learnLib1;
+    }
+
+    public void setLearnLib1(Collection<LearnLib1Server> learnLib1) {
+        this.learnLib1 = learnLib1;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "wordId", referencedColumnName = "wordId", nullable = false)
+    public WordLibServer getWordLib() {
+        return wordLib;
+    }
+
+    public void setWordLib(WordLibServer wordLib) {
+        this.wordLib = wordLib;
+    }
+}
